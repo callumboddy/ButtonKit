9405acd28c335b2df24c8392b908b9707a03213b
Callum Boddy <callum.boddy@gmail.com>
2016-08-20 07:52:35 +0100

    initial commit

---
+++ Buttons/Buttons_Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Buttons_Extension.swift
+//  Buttons
+//
+//  Created by Callum Boddy on 20/08/2016.
+//  Copyright © 2016 Callum Boddy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIButton {
+    func roundCorners(round: CGFloat) {
+        layer.cornerRadius = round
+    }
+}
