9be7aebeb2969b80e709996990c9771b18b12f9b
Callum Boddy <callum.boddy@gmail.com>
2016-08-20 11:02:57 +0100

    added tests for borders

--- ButtonsTests/ButtonsTests.swift
+++ ButtonsTests/ButtonsTests.swift
@@ -17,14 +17,37 @@ class RoundedButtonSpec: QuickSpec {
     override func spec() {
         let button: UIButton = UIButton()
         describe("rounding a button") {
-            it("has a default radius of 0") {
-                expect(button.layer.cornerRadius).to(equal(0))
+            context("by default") {
+                it("has a  radius of 0") {
+                    expect(button.layer.cornerRadius).to(equal(0))
+                }
             }
-
-            context("when i round it") {
+            
+            context("when i add apply a corner radius") {
                 it("updates the value to its new radius") {
                     button.roundCorners(2)
                     expect(button.layer.cornerRadius).to(equal(2))
+                    
+                }
+            }
+        }
+    }
+}
+
+class BorderedButtonSpec: QuickSpec {
+    override func spec() {
+        let button: UIButton = UIButton()
+        describe("adding a border to a button") {
+            context("by default") {
+                it("has a border width of 0") {
+                    expect(button.layer.borderWidth).to(equal(0))
+                }
+            }
+            
+            context("when i add border width") {
+                it("updates the value to its new border width") {
+                    button.layer.borderWidth = 4
+                    expect(button.layer.borderWidth).to(equal(4))
                 }
             }
         }
