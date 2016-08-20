62ed1048fe67f8787c5bf2dbea39181dbaf3d045
Callum Boddy <callum.boddy@gmail.com>
2016-08-20 07:52:35 +0100

    Initial Commit

---
+++ Buttons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Buttons
+//
+//  Created by Callum Boddy on 20/08/2016.
+//  Copyright © 2016 Callum Boddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ButtonsTests/ButtonsTests.swift
@@ -0,0 +1,36 @@
+//
+//  ButtonsTests.swift
+//  ButtonsTests
+//
+//  Created by Callum Boddy on 20/08/2016.
+//  Copyright © 2016 Callum Boddy. All rights reserved.
+//
+
+import XCTest
+@testable import Buttons
+
+class ButtonsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
