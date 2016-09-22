8a93bec603f58119d0ccfa8843434dc0f42b1221
Callum Boddy <callum.boddy@gmail.com>
2016-09-22 12:19:17 +0100

    framework ButtonKit

---
+++ ButtonKit/ButtonKit/ButtonStyler.swift
@@ -0,0 +1,58 @@
+//
+//  ButtonStyler.swift
+//  Buttons
+//
+//  Created by Callum Boddy on 22/08/2016.
+//  Copyright © 2016 Callum Boddy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ButtonStyler {
+    static let sharedInstance = ButtonStyler()
+    var colorScheme: ColorScheme
+    var fontScheme: FontScheme
+
+    init() {
+        colorScheme = ColorScheme()
+        fontScheme = FontScheme()
+    }
+
+    static func style(button: UIButton, color: ColorSchemeColor = .Default, style: ButtonStyle = .Flat, radius: ButtonRadius = .Rounded) {
+        let instance = self.sharedInstance
+        instance.applyDefaults(button)
+        let colorSchemeColor = instance.colorScheme.colorForButtonColor(color)
+        ButtonPainter.applyColorForButtonColor(button, color: colorSchemeColor)
+        if style == .Bordered {
+            ButtonBorder.applyBorderStyle(button, color: colorSchemeColor)
+        }
+        ButtonState.applyPressedState(button, style: style)
+        ButtonRadius.applyRadiusForButton(button, radius: radius)
+        ButtonTitle.applyTitleStyle(button, font: instance.fontScheme.font, color: colorSchemeColor, style: style)
+    }
+
+    func effect(button: UIButton, effect: ButtonEffectStyle) {
+        ButtonEffect.applyEffectForEffectStyle(button, effect: effect)
+    }
+
+    private func applyDefaults(button: UIButton) {
+        button.adjustsImageWhenHighlighted = false
+        button.adjustsImageWhenDisabled = false
+        button.clipsToBounds = true
+    }
+}
+
+public func style(button: UIButton, color: ColorSchemeColor = .Default, style: ButtonStyle = .Flat, radius: ButtonRadius = .Rounded) {
+    ButtonStyler.style(button, color: color, style: style, radius: radius)
+}
+
+extension UIButton {
+    public func style(color: ColorSchemeColor = .Default, style: ButtonStyle = .Flat, radius: ButtonRadius = .Rounded) {
+        ButtonStyler.style(self, color: color, style: style, radius: radius)
+    }
+    
+    public func effect(style: ButtonEffectStyle) {
+        ButtonEffect.applyEffectForEffectStyle(self, effect: style)
+    }
+}
